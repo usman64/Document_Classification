5a2f8233dd3931885c5bb68daa29e11a7c047881
Mohammad Obaid Ur Rehman <38375574+MohammadObaidUrRehman@users.noreply.github.com>
2019-07-08 17:15:08 +0500

    Code Formating
    Changed binarization to that in binarize.py
    More Generic Code 
    need to experiment with different psms

--- makestoplist.py
+++ makestoplist.py
@@ -1,21 +1,26 @@
 import pytesseract
 import cv2
 import re
+
 def tokenizeUsingSpaces(documentText):
 	pattern = r"\S+"
 	documentText=documentText.lower()
 	tokenizedWords = re.findall(pattern,alltext)
 	return tokenizedWords
 
-original = cv2.imread("RO-S.jpg",0)
-original = cv2.GaussianBlur(original,(5,5),0)
-ret2,original = cv2.threshold(original,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-alltext = pytesseract.image_to_string(original,config='-psm 12')
-alltext = alltext.lower()
-tokenizedWords=list(set(tokenizeUsingSpaces(alltext)))
-print(tokenizedWords)
+name  = input("Enter form name: ")
+fName = input("Enter file name: ")
 
-file = open("roswordlist.txt", "a")
+im_gray = cv2.imread(fName, 0)
+(thresh, im_bw) = cv2.threshold(im_gray, 120, 255, cv2.THRESH_BINARY)
+ret2,im_binary = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)
+alltext = pytesseract.image_to_string(im_binary,config= '--psm 12')
+alltext = alltext.lower()
+
+tokenizedWords=list(set(tokenizeUsingSpaces(alltext)))
+
+textFile=name+".txt"
+file = open(textFile, "a")
 for i in tokenizedWords:
 	temp = i + "\t"
 	file.write(temp)
