1d286b7f88f5888b1f796b7564164a5b7057f1d8
Mohammad Obaid Ur Rehman <38375574+MohammadObaidUrRehman@users.noreply.github.com>
2019-07-11 16:49:49 +0500

    Major changes made
    Classification Function 
    write to keywords file using storeKeyWord()
    need to automate input 
    need to add search

--- tess.py
+++ tess.py
@@ -1,16 +1,27 @@
-import os
-import re
-
-import cv2
-import pandas as pd
 import pytesseract
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+import re
+import os
+import cv2
 from PIL import Image
-from rake_nltk import Rake
+from nltk.tokenize import word_tokenize
+import pandas as pd
+from nltk.corpus import stopwords
+import measure_img_similarity3 as mis
+from os import listdir
+from os.path import isfile, join
+import numpy as np
 
-# pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\\tesseract.exe"
-#Use this for Windows systems --- 
+def classifier(filePath,folderPath):
+	allTemplates = [f for f in listdir(folderPath) if isfile(join(folderPath, f)) and '.png' in f and 'Template' in f]
+	allScores = []
+	allScores = np.array(allScores)
+	for x in allTemplates:
+		path = folderPath + '/' + x
+		tempscore = mis.sift_sim(path, filePath)
+		allScores = np.append(allScores, tempscore)
+	result = np.where(allScores == np.amax(allScores))
+	category = allTemplates[result[0][0]].split('Template')[0]
+	return category
 
 def tokenizeUsingSpaces(documentText):
 	pattern = r"\S+"
@@ -18,8 +29,8 @@ def tokenizeUsingSpaces(documentText):
 	tokenizedWords = re.findall(pattern,alltext)
 	return tokenizedWords
 
-def getStopWords(docType):
-	textFile = docType + '.txt'
+def getStopWords(docPath):
+	textFile = docPath
 	stopwords = []
 	file = open(textFile, 'r')
 	word = file.read()
@@ -35,9 +46,27 @@ def storeKeyDict(dictFile, imageName, wordList):
 		file.write(word + "\t")
 	file.close()
 
-imageName = input("Enter File Name: ")
-docType = input("Enter Document Type: ")
+def storeKeyWords(fileName, keyWordsList):
+	file = open('keywords.txt', 'r')
+	for i in file:
+		tempFName=i.split('\t')[0]
+		if tempFName==fileName:
+			return # keyword index already exits
+	file.close()
+	file = open('keywords.txt', 'a')
+	file.write('\n'+fileName+'\t')
+	for i in keyWordsList:
+		file.write(i+'\t')
+	file.close()
 
+# imageName = input("Enter   File   Path: ") #image to read and compare with template
+imageName = 'test/1.png'
+# folderPath = input("Enter Folder Path: ")
+folderPath = 'test'
+myclass = classifier(imageName, folderPath)
+print(myclass)
+# docPath   = input("Enter StopWord Path: ")# form name e.g. roge, ros, row
+docPath = myclass + '.txt'
 im_gray = cv2.imread(imageName, 0)
 (thresh, im_bw) = cv2.threshold(im_gray, 75, 255, cv2.THRESH_OTSU)
 ret2,im_binary = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)
@@ -45,25 +74,18 @@ alltext = pytesseract.image_to_string(im_binary,config= '--psm 12')
 
 alltext= alltext.lower()
 tokenizedWords=tokenizeUsingSpaces(alltext)
-stoplist = getStopWords(docType)
+print(tokenizedWords)
+stoplist = getStopWords(docPath)
 wordsFiltered = []
-ex = [0,1,2,3,4,5,6,7,8,9]
 for w in tokenizedWords:
-    if (w not in stoplist) and ((len(w) >2) or (w in ex)):
+    if w not in stoplist:
         wordsFiltered.append(w)
+fileName=imageName.split(os.sep)[-1]
 
-
-# filt = " ".join(wordsFiltered)
-# r = Rake()
-# r.extract_keywords_from_text(alltext)
-
-path =imageName.split(os.sep)
+storeKeyWords(fileName, wordsFiltered)
 
 df = pd.DataFrame(wordsFiltered)
-# df = pd.DataFrame(r.get_ranked_phrases())
 df = df.reset_index()
 df = df[0].value_counts().to_frame().reset_index()
 df.rename(columns={0:'Count'},inplace=True)
 print(df)
-wordsFiltered.insert(0,path[-1])
-print(wordsFiltered)
