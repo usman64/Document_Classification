b7698fa361de30f6210f903468714d8b303c6276
Mohammad Obaid Ur Rehman <38375574+MohammadObaidUrRehman@users.noreply.github.com>
2019-07-20 18:37:58 +0500

    Add files via upload
    Cleaned code and made functions

--- makestoplist.py
+++ makestoplist.py
@@ -1,30 +1,39 @@
 import pytesseract
-import cv2
 import re
+import cv2
 import os
+from os import listdir
+from os.path import isfile, join
 
-pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\\tesseract.exe"
-#Use this for Windows systems^ --- 
 def tokenizeUsingSpaces(documentText):
-    pattern = r"\S+"
-    documentText=documentText.lower()
-    tokenizedWords = re.findall(pattern,alltext)
-    return tokenizedWords
+	pattern = r"\S+"
+	documentText=documentText.lower()
+	tokenizedWords = re.findall(pattern,documentText)
+	return tokenizedWords
 
-name  = input("Enter form name: ")
-fName = input("Enter file path: ")
+def writer(fileName, tokenizedWords):
+	textFile=fileName+".txt"
+	file = open(textFile, "a")
+	for i in tokenizedWords:
+		if len(i)>2:
+			temp = i + "\t"
+			file.write(temp)
+	file.close()
 
-im_gray = cv2.imread(fName, 0)
-(thresh, im_bw) = cv2.threshold(im_gray, 75, 255, cv2.THRESH_BINARY)
-ret2,im_binary = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)
-alltext = pytesseract.image_to_string(im_binary,config= '--psm 12')
-alltext = alltext.lower()
+def imageProcessWords(imagePath):
+	im_gray = cv2.imread(imagePath, 0)
+	(thresh, im_bw) = cv2.threshold(im_gray, 120, 255, cv2.THRESH_BINARY)
+	ret2,im_binary = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)
+	alltext = pytesseract.image_to_string(im_binary,config= '--psm 12')
+	alltext = alltext.lower()
+	tokenizedWords=list(set(tokenizeUsingSpaces(alltext)))
+	return tokenizedWords
 
-tokenizedWords=list(set(tokenizeUsingSpaces(alltext)))
-
-textFile=name+".txt"
-file = open(textFile, "a")
-for i in tokenizedWords:
-    temp = i + "\t"
-    file.write(temp)
-file.close()
+def listForAll(folderPath):
+	allTemplates = [f for f in listdir(folderPath) if isfile(join(folderPath, f)) and '.png' in f and 'Template' in f]
+	for i in allTemplates:
+		imgPath = os.path.join(folderPath, i)
+		sWords = imageProcessWords(imgPath)
+		tempFName=i.split('Template')[0]
+		txtPath = os.path.join(folderPath,tempFName)
+		writer(txtPath, sWords)
